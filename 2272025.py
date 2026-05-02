fa186ecb532bede59f5b35a83c95d34b090cb3df
VIKNESHRAJ D <115133754+VIKNESHRAJD@users.noreply.github.com>
2026-05-02 23:55:53 +0530

    Add files via upload

---
+++ 2272025.py
@@ -0,0 +1,14 @@
+string = "HELLO, I AM GOOD"
+'''print(string)
+print(string.lower())
+print(string.upper())
+print(string.split())
+print(string.split("AM"))
+print(string.split(","))
+print(string.find("O"))
+print(string.count("HEL"))
+print(string.replace("AM", "WAS"))'''
+
+print("Hello" in string) #False
+
+print("HELLO" in string) #True
