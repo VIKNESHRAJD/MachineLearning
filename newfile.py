3b739fce23e43d75e99e5d1c95334da716089b43
VIKNESHRAJ D <115133754+VIKNESHRAJD@users.noreply.github.com>
2026-05-18 22:58:49 +0530

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,14 @@
+"""# 2. Subset
+A = {1, 2}
+B = {1, 2, 3}
+print("Is A subset of B?", A.issubset(B))
+
+print("------------------")
+"""
+
+# 4. Union, Intersection, Cross Product
+X = {1, 2}
+Y = {2, 3}
+
+# Union
+print("Union:", X | Y)
