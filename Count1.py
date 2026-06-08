ca655fb1400fcb97fa20cddce552c06be0a857f9
VIKNESHRAJ D <115133754+VIKNESHRAJD@users.noreply.github.com>
2026-06-08 22:35:32 +0530

    Add files via upload

---
+++ Count1.py
@@ -0,0 +1,19 @@
+'''A = [1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 6]
+
+for i in set(A):
+    print("Count of", i, ":", A.count(i))'''
+    
+    
+#3
+
+A = [1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 6]
+B = set(A)
+for i in B:
+	print(" Count of", i, ":", A.count(i)
+#4    
+'''x = [1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 6]
+b = [ ]
+for i in x:
+	if i not in b:
+		b.append(i)
+print(b)'''
---
+++ String.py
@@ -0,0 +1,23 @@
+'''write a program to do the following string operation in the sentence
+
+"Hi everyone, very good morning"
+
+i)slice the first 5 letters
+ii)slice the last 7 letters
+iii)find the occurance of 'e'
+iv)count number of o's
+v)check if the word 'very' appears
+vi) check if the word 'worry' not appears
+vii) Replace the word 'morning' by 'afternoon'''
+
+string = "Hi everyone,very good moring"
+
+#i Slice the first 5 letters
+print(string[0:5])
+
+#ii slice the last 7 letters
+print(string[7:0])
+
+#iii find the occurance of 'e'
+
+#iv count number of 0s
